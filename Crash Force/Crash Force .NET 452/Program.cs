91c5874f397f8e4545616b8b3020cee0dc70d4b4
Brandon Pen <brandon.pen@outlook.com>
2019-05-23 10:35:31 -0400

    - Renamed projects to reflect framework type - Created a .Net 4.5.2 project for good measure

--- Crash Force/Crash Force .NET 452/Program.cs
+++ Crash Force/Crash Force .NET 452/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Crash_Force.NET.Core22
 {
@@ -41,5 +45,4 @@ namespace Crash_Force.NET.Core22
                 sizeof(int));
         }
     }
-
 }
