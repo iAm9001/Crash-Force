1b10832c3b477458be1f1233046f4ff0eb3d858b
Brandon Pen <brandon.pen@outlook.com>
2019-05-23 10:00:17 -0400

    - Initial / final commit

---
+++ Crash Force/Crash Force/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Crash_Force
+{
+    using System.Diagnostics;
+    using System.Runtime.InteropServices;
+
+    /// <summary>
+    /// The crash force class contains logic for initiating an intentional system crash in Windows.  This is achieved by
+    /// setting the current process (this instance of the process CrashForce) to "Critical", which forces windows to crash
+    /// when the critical process terminates (once the <see cref="Main(string[])"/> method has completed it's execution).
+    /// This application must be executed with Admin privileges in order to successfully crash Windows.
+    /// </summary>
+    public class CrashForce
+    {
+        [DllImport("ntdll.dll", SetLastError = true)]
+        private static extern int NtSetInformationProcess(
+            IntPtr hProcess,
+            int processInformationClass,
+            ref int processInformation,
+            int processInformationLength);
+
+        /// <summary>
+        /// The main method.  Once this method finishes executing, Windows will crash.  Must be executed with Admin privileges.
+        /// </summary>
+        /// <param name="args">
+        /// The args.
+        /// </param>
+        static void Main(string[] args)
+        {
+            
+            int processInformationClass = 0x1D;
+            int processInformation = 1;
+
+            Process.EnterDebugMode();
+
+            NtSetInformationProcess(
+                Process.GetCurrentProcess().Handle,
+                processInformationClass,
+                ref processInformation,
+                sizeof(int));
+        }
+    }
+
+}
